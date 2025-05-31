0181f4f3a810513d76d195a43563b1eb1368c3c8
abdulkader33447 <abdulkader33447@gmail.com>
2025-05-31 17:32:49 +0600

    added

--- src/pages/myApplications/MyApplications.jsx
+++ src/pages/myApplications/MyApplications.jsx
@@ -7,12 +7,17 @@ import { myApplicationsPromise } from "../../api/applicationsApi";
 const MyApplications = () => {
   const { user } = useAuth();
 
+  console.log("token firebase token", user.accessToken);
+
+  // ---firebase token
+  // user.accessToken is firebase token
+
   return (
     <div>
       <ApplicationsStats />
       <Suspense fallback={"loading...."}>
         <ApplicationsList
-          myApplicationsPromise={myApplicationsPromise(user.email)}
+          myApplicationsPromise={myApplicationsPromise(user.email,user.accessToken)}
         />
       </Suspense>
     </div>
